cce73217baf348b828078daf0dc55f3f5eb5f7e6
i1gn1te <franeksos08@gmail.com>
2025-06-10 01:23:00 +0200

    Big Visual changes
    Added a lot of props, boss arena, and a few more assets

--- scenes/Hitbox/Hitbox.cs
+++ scenes/Hitbox/Hitbox.cs
@@ -3,16 +3,16 @@ using System;
 
 public partial class Hitbox : Area2D
 {
-    [Export]
-    public int Damage = 10;
-    public Vector2? AttackFromVector = null;
-    public override void _Ready()
-    {
-        CollisionLayer = 8;
-        CollisionMask = 0;
-    }
-    public void SetAttackFromVector(Vector2 attackVector)
-    {
-        this.AttackFromVector = attackVector;
-    }
+	[Export]
+	public int Damage = 10;
+	public Vector2? AttackFromVector = null;
+	public override void _Ready()
+	{
+		CollisionLayer = 8;
+		CollisionMask = 0;
+	}
+	public void SetAttackFromVector(Vector2 attackVector)
+	{
+		this.AttackFromVector = attackVector;
+	}
 }
